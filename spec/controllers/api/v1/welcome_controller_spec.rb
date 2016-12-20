54078d9e519c425706c00d23ecf1e6801ee24983
Malachi Gray <graymd.cfi@gmail.com>
2016-12-19 22:53:37 -0500

    welcome controller specs

--- spec/controllers/api/v1/welcome_controller_spec.rb
+++ spec/controllers/api/v1/welcome_controller_spec.rb
@@ -5,38 +5,42 @@ module Api
     RSpec.describe WelcomeController do
 
       describe '#index' do
-        context 'without a Authorization Header JWT' do
+
+        context 'when no Authorization Header is received' do
+          let(:not_authenticated_response) {["Not Authenticated No User ID In Token"]}
           it 'returns status unauthorized' do
             @request.headers['Authorization'] = ''
             get :index
             expect(response).to have_http_status 401
+            body = JSON.parse(response.body).with_indifferent_access
+            expect(body['errors']).to eq(not_authenticated_response)
           end
         end
 
-        context 'with a bad JWT' do
+        context 'when a bad JWT is received' do
+          let(:not_authenticated_response) {["Not Authenticated No User ID In Token"]}
           it 'returns status unauthorized' do
-            @request.headers['Authorization'] = 'xxxx.xxxx.xxxx'
+            @request.headers['Authorization'] = 'earer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.__axIF2iO7Ng_Z9DZ-XnGyrjpdELL4X5OOsQ-xSn-ho'
             get :index
+            body = JSON.parse(response.body).with_indifferent_access
+            expect(body['errors']).to eq(not_authenticated_response)
             expect(response).to have_http_status 401
           end
         end
 
-        context 'with a good JWT' do
+        context 'when a good JWT is received' do
           let!(:user) { User.create(email: "a@a.com", password: "password") }
+          let(:authenticated_response) {{logged_in: 'true'}.to_json}
           it 'returns status 200' do
             # binding.pry
             #:user=>{:id=>1, :email=>"a@a.com"}}
             @request.env["HTTP_AUTHORIZATION"] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.__axIF2iO7Ng_Z9DZ-XnGyrjpdELL4X5OOsQ-xSn-bo'
             get :index
+            expect(response.body).to eq authenticated_response
             expect(response).to have_http_status 200
           end
         end
-
-
-
-
       end
     end
   end
-
 end
