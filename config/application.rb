c0262a7859d08842a5bbf49bfd2b4e62c140214d
Malachi Gray <graymd.cfi@gmail.com>
2016-12-21 22:10:31 -0500

    remove rack-cors

--- config/application.rb
+++ config/application.rb
@@ -22,11 +22,5 @@ module Rails5ReactTodo
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     # todo updateorigins
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
   end
 end
